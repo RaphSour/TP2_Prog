f75dda1bf0805fa92f2a873070239ce1158bb03a
Gagnon01 <gagj1208@usherbrooke.ca>
2025-03-20 13:52:17 -0400

    Refactor LayerStatsWidget plugin structure and add main functionality

--- layerstatswidget/__init__.py
+++ layerstatswidget/__init__.py
@@ -32,5 +32,5 @@ def classFactory(iface):  # pylint: disable=invalid-name
     :type iface: QgsInterface
     """
     #
-    from .LayerStatsWidget import LayerStatsWidget
+    from .main import LayerStatsWidget
     return LayerStatsWidget(iface)
---
+++ layerstatswidget/main.py
@@ -0,0 +1,26 @@
+from PyQt5.QtWidgets import QAction, QDockWidget
+from qgis.PyQt.QtCore import Qt
+from .LayerStatsWidget import LayerStatsWidgetPanel
+
+class LayerStatsWidget:
+    def __init__(self, iface):
+        """Initialisation du plugin."""
+        self.iface = iface
+        self.dock_widget = None
+
+    def initGui(self):
+        """Création de l’interface utilisateur du plugin."""
+        self.action = QAction("Layer Stats Widget", self.iface.mainWindow())
+        self.action.triggered.connect(self.run)
+        self.iface.addPluginToMenu("LayerStatsWidget", self.action)
+
+    def unload(self):
+        """Suppression du plugin."""
+        self.iface.removePluginMenu("LayerStatsWidget", self.action)
+
+    def run(self):
+        """Affichage du widget."""
+        if not self.dock_widget:
+            self.dock_widget = LayerStatsWidgetPanel(self.iface)
+            self.iface.addDockWidget(Qt.RightDockWidgetArea, self.dock_widget)
+        self.dock_widget.show()
